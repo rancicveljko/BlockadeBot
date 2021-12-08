baae193a9a50cee2f3a41c4565b8716fac55e40c
Sneza <snezana.spasic@elfak.rs>
2021-12-08 21:00:11 +0100

    Dodat file Promenljive.py i Funcije.py => Bolje strukturiran projekat

---
+++ Promenljive.py
@@ -0,0 +1,24 @@
+# Trenutni igrac, True igra X, false igra O
+trenutniIgrac = False
+
+# Startne koordinate pesaka
+startX1 = tuple()
+startX2 = tuple()
+startO1 = tuple()
+startO2 = tuple()
+
+# pamte se trenutne kooridinate igraca
+X1 = tuple()
+X2 = tuple()
+O1 = tuple()
+O2 = tuple()
+# pamti se broj preostalih zidova
+brVertikalnihZidovaX = 0
+brHorizontalnihZidovaX = 0
+brVertikalinihZidovaO = 0
+brHorizontalnihZidovaO = 0
+# trenutni izgled matrica polja i zidova
+matPolja = []
+matZidovi = []
+hZidovi = []
+vZidovi = []
